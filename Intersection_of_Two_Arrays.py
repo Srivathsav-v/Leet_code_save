98f7df65e1774dde31c54ee5e3a1f7f771c77abb
Srivathsav Velpuri <149441423+Srivathsav-v@users.noreply.github.com>
2024-11-22 13:34:04 -0800

    leetcode 349
    convert each list into set (so we can remove duplicates)
    check if number from set 1 is in set 2 
    
    if so add it to a list and return it

---
+++ Intersection_of_Two_Arrays.py
@@ -0,0 +1,12 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1=set(nums1)
+        nums2=set(nums2)
+        nums=[]
+        for i in nums1:
+            if i in nums2:
+                nums.append(i)
+        
+        return nums
+                
+        
