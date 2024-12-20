5d7d2d0d271161fb692ab479700b42aa22471510
Srivathsav Velpuri <149441423+Srivathsav-v@users.noreply.github.com>
2024-12-19 18:53:49 -0800

    leetcode 3232
    solved it instantly

---
+++ Find_if_Digit_Game_Can_Be_Won.py
@@ -0,0 +1,13 @@
+class Solution:
+    def canAliceWin(self, nums: List[int]) -> bool:
+        sum1=0
+        sum2=0
+        for num in nums:
+            if num < 10:
+                sum1+=num
+            else:
+                sum2+=num
+        
+        if sum1==sum2:
+            return False
+        return True
